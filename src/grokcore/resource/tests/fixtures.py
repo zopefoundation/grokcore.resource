00e9481ae335a60e097871bae9d47dfed4cf8ed1
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2012-05-13 19:37:32 +0000

    Update imports, add fixtures for viewlet/contentprovider tests.

--- src/grokcore/resource/tests/fixtures.py
+++ src/grokcore/resource/tests/fixtures.py
@@ -1,6 +1,6 @@
+import grok
 import zope.interface
-import grokcore.view
-import grokcore.component
+import grokcore.layout
 import fanstatic
 
 lib = fanstatic.Library('grokcore.resource.tests', 'resources')
@@ -10,45 +10,103 @@ css_b = fanstatic.Resource(lib, 'b.css')
 css_c = fanstatic.Resource(lib, 'c.css')
 css_d = fanstatic.Resource(lib, 'd.css')
 
-grokcore.view.templatedir('templates')
+grok.templatedir('templates')
 
 class IMySuperIface(zope.interface.Interface):
     pass
 
-class MyView(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
-    grokcore.resource.include(css_a)
-    grokcore.view.template('index')
+class MyView(grok.View):
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_a)
+    grok.template('index')
+
+class DocTestView(grok.View):
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_a, css_b)
+    grok.template('index')
+    grok.name('doctest')
 
 class ViewWithOwnResource(MyView):
-    grokcore.resource.include(css_b)
+    grokcore.resource.resources(css_b)
 
-class ViewWithMultipleResources(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
-    grokcore.resource.include(css_c, css_b)
-    grokcore.view.template('index')
+class ViewWithMultipleResources(grok.View):
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_c, css_b)
+    grok.template('index')
 
-class ViewWithMultipleResourceCalls(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
-    grokcore.resource.include(css_c, css_b)
-    grokcore.resource.include(css_a)
-    grokcore.view.template('index')
+class ViewWithMultipleResourceCalls(grok.View):
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_c, css_b)
+    grokcore.resource.resources(css_a)
+    grok.template('index')
 
-class ViewWithResourceInUpdate(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
-    grokcore.resource.include(css_a)
-    grokcore.view.template('index')
+class ViewWithResourceInUpdate(grok.View):
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_a)
+    grok.template('index')
 
     def update(self):
         css_d.need()
 
-class MySuperView(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
+class MySuperView(grok.View):
+    grok.context(zope.interface.Interface)
     zope.interface.implements(IMySuperIface)
-    grokcore.view.template('index')
+    grok.template('index')
 
-class MySuperViewWithResource(grokcore.view.View):
-    grokcore.component.context(zope.interface.Interface)
+class MySuperViewWithResource(grok.View):
+    grok.context(zope.interface.Interface)
     zope.interface.implements(IMySuperIface)
-    grokcore.resource.include(css_a)
-    grokcore.view.template('index')
+    grokcore.resource.resources(css_a)
+    grok.template('index')
+
+# Layout.
+
+class MyLayout(grokcore.layout.Layout):
+    grok.context(zope.interface.Interface)
+    grok.template('layout')
+    grokcore.resource.resources(css_a)
+
+class MyPage(grokcore.layout.Page):
+    grok.template('page')
+    grok.context(zope.interface.Interface)
+    grokcore.resource.resources(css_b)
+
+# Viewlet.
+class ViewWithViewletManager(grok.View):
+    grok.context(zope.interface.Interface)
+    grok.template('viewletmgr')
+
+class MyViewletManager(grok.ViewletManager):
+    grok.name('foo')
+    grok.view(ViewWithViewletManager)
+    grok.context(zope.interface.Interface)
+
+class ViewletA(grok.Viewlet):
+    grok.viewletmanager(MyViewletManager)
+    grokcore.resource.resources(css_a)
+    grok.context(zope.interface.Interface)
+
+    def render(self):
+        return u'aaa'
+
+class ViewletC(grok.Viewlet):
+    grok.viewletmanager(MyViewletManager)
+    grokcore.resource.resources(css_c)
+    grok.context(zope.interface.Interface)
+
+    def render(self):
+        return u'ccc'
+
+# ContentProvider.
+class ViewWithContentProvider(grok.View):
+    grok.context(zope.interface.Interface)
+    grok.template('contentprovider')
+
+class MyContentProvider(grok.ContentProvider):
+    grok.name('bar')
+    grok.view(ViewWithContentProvider)
+    grokcore.resource.resources(css_b)
+    grok.context(zope.interface.Interface)
+
+    def render(self):
+        return u'bar'
